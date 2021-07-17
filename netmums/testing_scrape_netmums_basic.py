ab64ad3d7435ec4996d6dcc61c5c4f6c717b2f37
Sherman <saline@ucsd.edu>
2021-07-17 13:45:35 +0200

    misc notebook files, temporary files

---
+++ netmums/testing_scrape_netmums_basic.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# @Author: sma
+# @Date:   2021-04-21 13:18:20
+# @Last Modified by:   sma
+# @Last Modified time: 2021-04-22 19:59:14
+
+import scrapehelpers #TODO not sure if this works, maybe just not working in REPL,
+
+### TESTING
+
+#make list of urls (uses scrapehelpers helpoer functions)
+templ = netmum_urls(make_combo_list(problems, foods))
+
+
+#make the request for each url with a time in between. returns a list of request objs.
+#use 0.05s sleep just in case the site has some type of protection, don't want ot get blocked.
+query = [requests.post(url).text for url in templ[1:3] if time.sleep(0.05) is None]
+
+
+#def
+#making thing to get links from search result page
+
+
+
+
+
+#def
+#soup to get the number of results
+
+
+
+
+[link.find('a').get('href') for link in soup.find_all('h3', {'class', 'card__title'})]
+
+
+
+#rewriting.
+
+
